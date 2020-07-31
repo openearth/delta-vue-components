30edeccfbd7c1e3891bb64df9d0c30e7fd52f04d
Bob Bol <bob@voorhoede.nl>
2020-07-31 13:40:49 +0200

    basic setup for component library

--- component-library/src/App.vue
+++ component-library/src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <map-layer-controls />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MapLayerControls from '@/components/MapLayerControls/MapLayerControls';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    MapLayerControls
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
