de725adebcc699c5dcf2584e0443e8e027c53793
Bob Bol <bob@voorhoede.nl>
2020-07-31 15:55:55 +0200

    deltares vuetify theme as reusable theme

--- demo-app/src/plugins/vuetify.js
+++ demo-app/src/plugins/vuetify.js
@@ -1,7 +1,11 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import { DeltaVuetifyThemes as themes } from 'component-library';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes
+  },
 });
