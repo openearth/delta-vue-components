5e1635d813246731138f022e189cbfd8a7a803ad
Peter Goes <petergoes@gmail.com>
2020-11-03 13:36:54 +0100

    Update demo project to use LayerListControls component

--- demo-app/src/components/AppSidebar/AppSidebar.vue
+++ demo-app/src/components/AppSidebar/AppSidebar.vue
@@ -1,21 +1,23 @@
 <template>
-<v-navigation-drawer
-  clipped
-  app
-  permanent
-  width="360"
->
-  <h2>
-    Welcome
-  </h2>
-  <map-layer-controls />
-</v-navigation-drawer>
+  <v-navigation-drawer
+    clipped
+    app
+    permanent
+    width="360"
+  >
+    <h2>
+      Welcome
+    </h2>
+    <map-layer-controls />
+    <layer-list-controls :layers="[]" />
+  </v-navigation-drawer>
 </template>
 
 <script>
-import { MapLayerControls } from 'component-library';
+import { MapLayerControls, LayerListControls } from 'component-library';
 export default {
   components: {
+    LayerListControls,
     MapLayerControls,
   },
 };
