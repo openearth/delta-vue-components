d5dafd53230126a6f2511227ff30fafcd3f58115
Peter Goes <petergoes@gmail.com>
2020-11-19 13:30:20 +0100

    Add AppSidebar to @deltares/vue-components

--- packages/vue-components/lib/vue-components.js
+++ packages/vue-components/lib/vue-components.js
@@ -1,12 +1,13 @@
 import AppHeader from './components/AppHeader'
 import AppShell from './components/AppShell'
-import MapLayerControls from './components/MapLayerControls';
+import AppSidebar from './components/AppSidebar'
 import LayerListControls from './components/LayerListControls';
 import MapLayerControls from './components/MapLayerControls';
 
 export {
   AppHeader,
   AppShell,
+  AppSidebar,
   LayerListControls,
   MapLayerControls,
 };
