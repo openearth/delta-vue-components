a8d809d81d2476a86622ba5eedd49b4d678b9bd7
Peter Goes <petergoes@gmail.com>
2020-11-19 13:28:46 +0100

    Add AppShell to @deltares/vue-components

--- packages/vue-components/lib/vue-components.js
+++ packages/vue-components/lib/vue-components.js
@@ -1,7 +1,10 @@
+import AppShell from './components/AppShell'
 import MapLayerControls from './components/MapLayerControls';
 import LayerListControls from './components/LayerListControls';
+import MapLayerControls from './components/MapLayerControls';
 
 export {
+  AppShell,
   LayerListControls,
   MapLayerControls,
 };
