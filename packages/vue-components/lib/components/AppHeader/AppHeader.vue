35a86742741a3eac8b7459a49cab9ca7799b9b92
Peter Goes <petergoes@gmail.com>
2020-11-19 13:29:43 +0100

    Add Appheader to @deltares/vue-components

---
+++ packages/vue-components/lib/components/AppHeader/AppHeader.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app-bar
+    app
+    class="app-header"
+    :color="color"
+    :dark="theme === 'dark'"
+  >
+    <slot name="left"/>
+    <v-toolbar-title v-if="leftSlotIsFilled === false">
+      {{ title }}
+    </v-toolbar-title>
+
+    <v-spacer />
+
+    <slot name="right" />
+
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    theme: {
+      type: String,
+      default: 'dark',
+      validator: value => value === 'dark' || value === 'light'
+    },
+    color: {
+      type: String,
+      default: 'primary',
+    }
+  },
+  computed: {
+    leftSlotIsFilled() {
+      return !!this.$slots['left']
+    }
+  },
+}
+</script>
+
+<style>
+.app-header {
+  left: 0 !important;
+}
+</style>
--- packages/vue-components/lib/vue-components.js
+++ packages/vue-components/lib/vue-components.js
@@ -1,9 +1,11 @@
+import AppHeader from './components/AppHeader'
 import AppShell from './components/AppShell'
 import MapLayerControls from './components/MapLayerControls';
 import LayerListControls from './components/LayerListControls';
 import MapLayerControls from './components/MapLayerControls';
 
 export {
+  AppHeader,
   AppShell,
   LayerListControls,
   MapLayerControls,
