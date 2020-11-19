1c2bc93574029efe00ce49af790496ac294e6d98
Peter Goes <petergoes@gmail.com>
2020-11-19 13:34:17 +0100

    Add About route to display multiple pages in a app

--- packages/demo-app/src/views/Home.vue
+++ packages/demo-app/src/views/Home.vue
@@ -1,19 +1,39 @@
 <template>
-<div style="height: 100%;">
-  <app-sidebar />
-  <mapbox-map />
-</div>
+  <app-sidebar>
+    <layer-list-controls :layers="layers" />
+  </app-sidebar>
 </template>
 
 <script>
-import AppSidebar from '@/components/AppSidebar/AppSidebar';
-import MapboxMap from '@/components/MapboxMap/MapboxMap';
+import { AppSidebar, LayerListControls } from '@deltares/vue-components';
 
 export default {
   name: 'Home',
   components: {
     AppSidebar,
-    MapboxMap,
+    LayerListControls,
   },
+  data: () => ({
+    layers: [
+      {
+        "id": "geological-layout1",
+        "name": "Geological Layout",
+        "children": [
+          {
+            "id": "plasvorming_herstelkosten",
+            "name": "Plasvorming Herstelkosten",
+            "layer": "ra2ce:plasvorming_herstelkosten",
+            "url": "https://ra2ce.openearth.eu/geoserver/ows"
+          },
+          {
+            "id": "plasvorming_stremmingskosten",
+            "name": "Plasvorming Stremmingskosten",
+            "layer": "ra2ce:plasvorming_stremmingskosten",
+            "url": "https://ra2ce.openearth.eu/geoserver/ows"
+          },
+        ]
+      }
+    ]
+  })
 };
 </script>
