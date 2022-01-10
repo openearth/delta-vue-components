60677e4b4a8d7e043779102e654b1dc70c3c725f
Bob Bol <studio.innua@gmail.com>
2022-01-10 11:48:29 +0100

    add raster layers

--- packages/demo-app/src/App.vue
+++ packages/demo-app/src/App.vue
@@ -3,19 +3,28 @@
     <router-link slot="header-right" to="/"><v-btn text>Home</v-btn></router-link>
     <router-link slot="header-right" to="/about"><v-btn text>About</v-btn></router-link>
 
-    <legal-dialog
+    <!-- <legal-dialog
       title="Legal"
       buttonText="Accept"
       storage="none"
       :body="legalText"
       :checkboxes="['I accept the above statement', 'I agree to the use of cookies']"
-    />
+    /> -->
 
     <mapbox-map
       slot="map"
       :access-token="accessToken"
     >
-      <mapbox-wms-layer v-for="layer in wmsLayers" :key="layer.id" :layer="layer" />
+      <mapbox-wms-layer
+        v-for="layer in wmsLayers"
+        :key="layer.id"
+        :layer="layer"
+      />
+      <v-mapbox-layer
+        v-for="layer in rasterLayers"
+        :key="layer.id"
+        :options="layer"
+      />
     </mapbox-map>
 
   </app-shell>
@@ -25,7 +34,6 @@
 import { mapState } from 'vuex'
 import { AppShell, MapboxMap, MapboxWmsLayer, LegalDialog } from '@deltares/vue-components'
 
-
 export default {
   components: {
     AppShell,
@@ -33,14 +41,17 @@ export default {
     MapboxWmsLayer,
     LegalDialog,
   },
+
   data: () => ({
     accessToken: process.env.VUE_APP_MAPBOX_TOKEN,
     legalText: 'lorem <b>ipsum</b> dolor sit amet, consectetur adipisicing elit. Quibusdam iure earum, quidem, dolorem, ex eveniet labore illo quis porro accusamus ad nisi ab nam. Tempora nisi corrupti a cumque alias.'
   }),
+
   computed: {
     ...mapState({
-      wmsLayers: ({ map }) => map.wmsLayers
+      wmsLayers: ({ map }) => map.wmsLayers,
+      rasterLayers: ({ map }) => map.rasterLayers,
     }),
-  }
+  },
 }
 </script>
--- packages/demo-app/src/store/modules/map.store.js
+++ packages/demo-app/src/store/modules/map.store.js
@@ -3,11 +3,37 @@ export default {
 
   state: () => ({
     wmsLayers: [],
+    rasterLayers: [
+      {
+        id: 'satellite-natural-video',
+        type: 'raster',
+        layout: {
+          visibility: 'visible'
+        },
+        source: {
+          type: 'video-tiled',
+          tiles: [
+            'https://storage.googleapis.com/vegetatiemonitor/satellite-natural-video/{z}/{x}/{y}.webm'
+          ],
+          tileSize: 512,
+          durationSec: 2.2,
+          dateBegin: '2000-01-01',
+          dateEnd: '2022-01-01',
+          maxzoom: 14,
+          minzoom: 9,
+          scheme: 'xyz',
+          geometry: []
+        }
+      }
+    ],
   }),
 
   mutations: {
     setWmsLayers(state, payload) {
-      state.wmsLayers = payload
+      state.wmsLayers = payload;
+    },
+    setRasterLayers(state, payload) {
+      state.rasterLayers = payload;
     },
   },
 }
